392db41ff205bda4bf6e56df597063f5149eb6f2
Sahil Dhumale <sahildhumale11@gmail.com>
2024-04-28 17:25:31 +0530

    Added UI and Editor features

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import template from '../template.json'
+import template from '../template.json';
+import './canvas.css';
 class Canvas extends Component {
-    
+
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
@@ -15,99 +16,140 @@ class Canvas extends Component {
     this.updateCanvas();
   }
 
+  updateCaption(ctx, text1) {
+    const position_x = template.caption.position.x;
+    const position_y = template.caption.position.y;
+    const max_characters_per_line = 31;
+    const font_size = template.caption.font_size;
+    const alignment = template.caption.alignment;
+    const text_color = template.caption.text_color;
+    const padding = 5;
+    ctx.font = `${font_size}pt bold Kremlin Pro Web `;
+    ctx.fillStyle = text_color;
+    ctx.textAlign = alignment;
+
+    // split text into words
+    const words = text1.split(' ');
+
+    // initialize line index and current line
+    let lineIndex = 0;
+    let currentLine = '';
+
+    // loop through words and add them to the current line
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+
+        // check if adding the word to the current line exceeds the maximum characters per line
+        if (currentLine.length + word.length + 1 > max_characters_per_line) {
+            // move to the next line
+            ctx.fillText(currentLine.trim(), position_x, position_y + lineIndex * (font_size + padding));
+            currentLine = word + ' ';
+            lineIndex++;
+        } else {
+            // add the word to the current line
+            currentLine += word + ' ';
+        }
+    }
+
+    // fill the remaining text on the current line
+    ctx.fillText(currentLine.trim(), position_x, position_y + lineIndex * (font_size + padding));
+}
+updateCTA(ctx, text2, rect) {
+  if (text2 != null) {
+    ctx.beginPath();
+    const radius = 10; // Set the radius of the rounded corners
+    ctx.moveTo(rect.x, rect.y + radius);
+    ctx.lineTo(rect.x, rect.y + rect.height - radius);
+    ctx.quadraticCurveTo(rect.x, rect.y + rect.height, rect.x + radius, rect.y + rect.height);
+    ctx.lineTo(rect.x + rect.width - radius, rect.y + rect.height);
+    ctx.quadraticCurveTo(rect.x + rect.width, rect.y + rect.height, rect.x + rect.width, rect.y + rect.height - radius);
+    ctx.lineTo(rect.x + rect.width, rect.y + radius);
+    ctx.quadraticCurveTo(rect.x + rect.width, rect.y, rect.x + rect.width - radius, rect.y);
+    ctx.lineTo(rect.x + radius, rect.y);
+    ctx.quadraticCurveTo(rect.x, rect.y, rect.x, rect.y + radius);
+    ctx.fillStyle = rect.background_color;
+    ctx.fill();
+    ctx.closePath();
+    ctx.font = '30pt bold Kremlin Pro Web';
+    ctx.fillStyle = rect.text_color;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(text2, rect.x + rect.width / 2, rect.y + rect.height / 2);
+  }
+}
+  
 
   updateCanvas() {
     const canvas = this.canvasRef.current;
     const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // const background = new Image();
-    // background.src = "../assests/bg.jpeg";
-    // background.onload = () => {
-    //   ctx.drawImage(background, canvas.width, canvas.height);
-    // };
 
-    const { imageSrc, text1, text2, bgcolor} = this.props;
-    
+    const { imageSrc, text1, text2, bgcolor } = this.props;
     canvas.style.background = bgcolor;
 
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "black";
-    ctx.fillText(text1, 50, 50);
-    ctx.fillText(text2, 50, 100);
 
     const designPattern = new Image();
-    designPattern.src = template.urls.design_pattern;
-    designPattern.onload = () => {
-      ctx.drawImage(designPattern, 0, 0);
-    };
-
-    
-
     const mask = new Image();
-    mask.src = template.urls.mask;
-    mask.onload = () => {
-      ctx.drawImage(mask, 0, 0);
-
-    };
-
     const maskStroke = new Image();
-    maskStroke.src = template.urls.stroke;
-    maskStroke.onload = () => {
-      ctx.drawImage(maskStroke, 0, 0);
-    };
-    
     const image = new Image();
+
+    designPattern.src = template.urls.design_pattern;
+    mask.src = template.urls.mask;
+    maskStroke.src = template.urls.stroke;
     image.src = imageSrc;
+
+
     const x = template.image_mask.x;
     const y = template.image_mask.y;
     const iwidth = template.image_mask.width;
     const iheight = template.image_mask.height;
+    setInterval(function () {
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.drawImage(designPattern, 0, 0);
+      ctx.drawImage(mask, 0, 0);
+      ctx.drawImage(maskStroke, 0, 0);
+    }, 16.666)
     image.onload = () => {
-        ctx.drawImage(image, x,y,iwidth,iheight);
-        // ctx.globalCompositeOperation='source-out'
+      ctx.globalCompositeOperation = 'source-over';
+      ctx.drawImage(image, x, y, iwidth, iheight);
+
     };
+    ctx.globalCompositeOperation = 'source-over';
+
+    this.updateCaption(ctx, text1);
+    // this.updateCTA(ctx,text2); 
+
+    // The rectangle should have x,y,width,height properties
+    var rect = {
+      x: template.cta.position.x,
+      y: template.cta.position.y,
+      width: template.cta.position.x*1.5,
+      height: template.cta.position.y/3.4,
+      text_color:template.cta.text_color,
+      background_color:template.cta.background_color
+    };
+    this.updateCTA(ctx,text2,rect);
+    
   }
 
-  
-  downloadCanvas() {
-    const canvas = this.canvasRef.current;
-    const dataUrl = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-    link.href = dataUrl;
-    link.download = "canvas_image.png";
-    link.click();
-  }
-  
 
   render() {
     return (
-        <div>
-      <canvas
-        className="my-canvas
-        ref={this.canvasRef}
-        width="1080px"
-        height="1080px"
-        style={{
-          display: "block",
-          margin: "50% auto",
-          height: 400,
-          width: 400
-        }}
-      />
-      <button onClick={this.downloadCanvas}>Download Canvas</button>
+      <div>
+        <canvas
+          ref={this.canvasRef}
+          width="1080px"
+          height="1080px"
+          style={{
+            display: "block",
+            margin: "50% auto",
+            height: 400,
+            width: 400
+          }}
+        />
       </div>
     );
   }
 }
 
 export default Canvas;
-
-
-
-
-    // const bgImage = new Image();
-    // bgImage.src = "../assets/bg.jpg";
-    // console.log(bgImage)
-    // bgImage.onload = () => {
-    //     ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
-    // };
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import Canvas from "./Canvas";
-// import wavyline from '../assets/wavyline.webp';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { SketchPicker } from "react-color";
-
 
 
 
@@ -14,11 +11,13 @@ const Editor = () => {
   const [text2, setText2] = useState("");
   const [bgcolor, setBgcolor] = useState("#0369A1");
   const [showColorPicker, setShowColorPicker] = useState(false);
+
   const [colorHistory, setColorHistory] = useState([]);
   const handleChangeComplete = (color) => {
     // setActiveColor(color.hex);
     setBgcolor(color.hex); // Update bgcolor state
     setColorHistory((prevColors) => [...prevColors, color.hex]);
+    setShowColorPicker(false);
   };
 
   const handleColorSelect = (color) => {
@@ -41,89 +40,72 @@ const Editor = () => {
   return (
     <div className="container-fluid">
       <div className="row">
+        <div className="col my-canvas">
+          <Canvas
+            className="border"
+            imageSrc={imageSrc}
+            text1={text1}
+            text2={text2}
+            bgcolor={bgcolor}
+          />
+        </div>
         <div className="col">
-          <div className="canvas">
-            <Canvas
-              className="border"
-              imageSrc={imageSrc}
-              text1={text1}
-              text2={text2}
-              bgcolor={bgcolor}
-            />
+          <div className=" editor">
+            <h2 align="center">Ad customization</h2>
+            <h4 align="center">
+              Customize your ad and get the templates accordingly
+            </h4>
+            <br />
+            <br />
+
+
+            <label for="images" class="drop-container" id="dropcontainer">
+              <input type="file" id="images" accept="image/*" onChange={handleImageChange} required />
+            </label>
+
+            <br />
+            <br />
+            <hr />
+            <h6 align="center">Edit Contents</h6>
+            <br />
+            <br />
+            <div className="form-floating mb-3">
+              <input type="text" className="form-control" id="floatingcaption" placeholder="Your Caption"  value={text1}
+                onChange={(e) => setText1(e.target.value)}/>
+              <label htmlFor="floatingcaption">Caption</label>
+            </div>
+            <div className="form-floating">
+              <input type="text" className="form-control" id="floatingcta" placeholder="Your Call To Action" value={text2}
+                onChange={(e) => setText2(e.target.value)}/>
+              <label htmlFor="floatingcta">CTA</label>
+            </div>
+            <br></br>
+            <div className="color-picker">
+
+              <div className="color-history">
+                {colorHistory.slice(-5).map((color, index) => (
+                  <div
+                    key={index}
+                    style={{ backgroundColor: color }}
+                    className="color-swatch"
+                    onClick={() => handleColorSelect(color)}
+                  />
+                ))}
+                {colorHistory.length >= 0 && (
+                  <button className="add-color" onClick={() => setShowColorPicker(true)}>
+                    +
+                  </button>
+                )}
+              </div>
+              {showColorPicker && (
+                <div className="position-absolute" style={{ zIndex: 9999 }}>
+                  <SketchPicker color={bgcolor} onChange={handleChangeComplete} />
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        <div className="col" id="editor">
-          <h2 align="center">Ad customization</h2>
-          <h4 align="center">
-            Customize your ad and get the templates accordingly
-          </h4>
-          <br />
-          <br />
-          
-          <div className="input-group mb-3">
-            <input type="file" id="image" accept="image/*" onChange={handleImageChange} />
-            </div>
-            {/* <div className="mb-3">
-              <input type="file" id="image" accept="image/*" onChange={handleImageChange} />
-            </div> */}
-          <br />
-          <br />
-          <hr />
-          <h6 align="center">Edit Contents</h6>
-          <br />
-          <br />
-          <div className="form-floating mb-3">
-            <label htmlFor="text1"></label>
-            <input
-              type="text"
-              id="text1"
-              className="form-control"
-              placeholder="Your Caption"
-              value={text1}
-              onChange={(e) => setText1(e.target.value)}
-            />
-          </div>
-          <div className="form-floating mb-3">
-            <label htmlFor="text2"></label>
-            <input
-              type="text"
-              id="text2"
-              className="form-control"
-              placeholder="CTA"
-              value={text2}
-              onChange={(e) => setText2(e.target.value)}
-            />
-          </div>
-          <div className="color-picker">
-            <div className="color-history">
-              {colorHistory.slice(-5).map((color, index) => (
-                <div
-                  key={index}
-                  style={{ backgroundColor: color }}
-                  className="color-swatch"
-                  onClick={() => handleColorSelect(color)}
-                />
-              ))}
-            </div>
-
-          <button
-            onClick={() => setShowColorPicker(!showColorPicker)}
-            className="btn btn-primary"
-          >
-            Choose your colour
-          </button>
-          {showColorPicker && (
-            <div className="position-absolute" style={{ zIndex: 9999 }}>
-              <SketchPicker color={bgcolor} onChange={handleChangeComplete} />
-            </div>
-          )}
-          {/* <button onClick="sampleColor()">Pippette icon</button> */}
-        </div>
-        
-
-        {/* Render Canvas component and pass props */}
       </div>
-    </div>
     </div>
   );
 };
